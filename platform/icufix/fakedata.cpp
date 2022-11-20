07307813b6d967e86f87fb11c8843724993a3d80
Marten Richter <marten.richter@tu-berlin.de>
2022-11-20 18:12:45 +0000

    Switch back to manual icu build and fake data

---
+++ platform/icufix/fakedata.cpp
@@ -0,0 +1,8 @@
+#include "unicode/putil.h"
+#include "unicode/udata.h"
+#include "unicode/uversion.h"
+#include "ucmndata.h"
+
+extern "C" const DataHeader U_DATA_API U_ICUDATA_ENTRY_POINT;
+
+const DataHeader  U_ICUDATA_ENTRY_POINT = { {0,0,0}, {0,0,0,0,0,0,0,0,0}};
