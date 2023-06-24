b86aa8cbac8915e1c20167c6356988a35c49c195
Marten Richter <marten.richter@tu-berlin.de>
2023-06-24 06:39:28 +0000

    Adapt to changes upstream

--- src/http3clientsession.h
+++ src/http3clientsession.h
@@ -17,40 +17,44 @@
 #include "quiche/quic/core/http/quic_spdy_client_session.h"
 #include "src/http3clientstream.h"
 
-namespace quic {
+namespace quic
+{
 
-class Http3ClientSession : public QuicSpdyClientSession {
- public:
-  Http3ClientSession(const QuicConfig& config,
-                          const ParsedQuicVersionVector& supported_versions,
-                          QuicConnection* connection,
-                          const QuicServerId& server_id,
-                          QuicCryptoClientConfig* crypto_config,
-                          QuicClientPushPromiseIndex* push_promise_index,
-                          bool drop_response_body);
-  Http3ClientSession(const QuicConfig& config,
-                          const ParsedQuicVersionVector& supported_versions,
-                          QuicConnection* connection,
-                          const QuicServerId& server_id,
-                          QuicCryptoClientConfig* crypto_config,
-                          QuicClientPushPromiseIndex* push_promise_index,
-                          bool drop_response_body, bool enable_web_transport);
+  class Http3ClientSession : public QuicSpdyClientSession
+  {
+  public:
+    Http3ClientSession(const QuicConfig &config,
+                       const ParsedQuicVersionVector &supported_versions,
+                       QuicConnection *connection,
+                       const QuicServerId &server_id,
+                       QuicCryptoClientConfig *crypto_config,
+                       QuicClientPushPromiseIndex *push_promise_index,
+                       bool drop_response_body);
+    Http3ClientSession(const QuicConfig &config,
+                       const ParsedQuicVersionVector &supported_versions,
+                       QuicConnection *connection,
+                       const QuicServerId &server_id,
+                       QuicCryptoClientConfig *crypto_config,
+                       QuicClientPushPromiseIndex *push_promise_index,
+                       bool drop_response_body, bool enable_web_transport);
 
-  std::unique_ptr<QuicSpdyClientStream> CreateClientStream() override;
-  bool ShouldNegotiateWebTransport() override;
-  HttpDatagramSupport LocalHttpDatagramSupport() override;
+    std::unique_ptr<QuicSpdyClientStream> CreateClientStream() override;
+    WebTransportHttp3VersionSet LocallySupportedWebTransportVersions()
+        const override;
+    HttpDatagramSupport LocalHttpDatagramSupport() override;
 
-  void set_on_interim_headers(
-      std::function<void(const spdy::Http2HeaderBlock&)> on_interim_headers) {
-    on_interim_headers_ = std::move(on_interim_headers);
-  }
+    void set_on_interim_headers(
+        std::function<void(const spdy::Http2HeaderBlock &)> on_interim_headers)
+    {
+      on_interim_headers_ = std::move(on_interim_headers);
+    }
 
- private:
-  std::function<void(const spdy::Http2HeaderBlock&)> on_interim_headers_;
-  const bool drop_response_body_;
-  const bool enable_web_transport_;
-};
+  private:
+    std::function<void(const spdy::Http2HeaderBlock &)> on_interim_headers_;
+    const bool drop_response_body_;
+    const bool enable_web_transport_;
+  };
 
-}  // namespace quic
+} // namespace quic
 
-#endif  // QUICHE_QUIC_TOOLS_QUIC_SIMPLE_CLIENT_SESSION_H_
+#endif // QUICHE_QUIC_TOOLS_QUIC_SIMPLE_CLIENT_SESSION_H_
