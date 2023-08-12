b8fae05e72466b65902b46326b99739221ec8d0b
Marten Richter <marten.richter@freenet.de>
2023-08-12 08:06:40 +0200

    Remove workaround

--- platform/base/strings/utf_ostream_operators.h
+++
@@ -1,5 +0,0 @@
-// adds string to the includes of google url
-#include <string>
-#warning "Fake utf_stream_operators.h included"
-
-#include "third_party/googleurl/base/strings/utf_ostream_operators.h"
