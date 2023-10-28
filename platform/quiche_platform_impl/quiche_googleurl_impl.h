1bdcb9bd4b29ebed7564c8baaadd84298f8f12df
Marten Richter <marten.richter@tu-berlin.de>
2023-10-28 15:57:48 +0000

    Create platform impl googleurl

---
+++ platform/quiche_platform_impl/quiche_googleurl_impl.h
@@ -0,0 +1,8 @@
+// only for redirects
+
+#ifndef NET_QUICHE_COMMON_PLATFORM_IMPL_QUICHE_GOOGLEURL_IMPL_H_
+#define NET_QUICHE_COMMON_PLATFORM_IMPL_QUICHE_GOOGLEURL_IMPL_H_
+
+#include "third_party/quiche/quiche/common/platform/default/quiche_platform_impl/quiche_googleurl_impl.h"
+
+#endif
