7e95f0e41d3c7a562da041d6f4ecbedc180343c1
Marten Richter <marten.richter@tu-berlin.de>
2023-12-16 07:30:18 +0000

    New test for sending and receiving big buffers

--- main/test/fixtures/known-bytes.js
+++ main/test/fixtures/known-bytes.js
@@ -13,3 +13,32 @@ export const KNOWN_BYTES_LENGTH = KNOWN_BYTES.reduce(
   (accumulator, currentValue) => accumulator + currentValue.length,
   0
 )
+
+const createBytesChunk = function (length) {
+  const workArray = new Array(length / 2)
+  for (let i = 0; i < length / 4; i++) {
+    workArray[2 * i + 1] = length % 0xffff
+    workArray[2 * i] = i
+  }
+  const helper = new Uint16Array(workArray)
+  const toreturn = new Uint8Array(
+    helper.buffer,
+    helper.byteOffset,
+    helper.byteLength
+  )
+  return toreturn
+}
+
+export const KNOWN_BYTES_LONG = [
+  createBytesChunk(60000), // 96, 234
+  createBytesChunk(12), // 0, 12
+  createBytesChunk(50000), // 195, 80
+  createBytesChunk(1600), // 6, 64
+  createBytesChunk(20000), // 78, 32
+  createBytesChunk(30000) // 117, 48
+]
+
+export const KNOWN_BYTES_LONG_LENGTH = KNOWN_BYTES_LONG.reduce(
+  (accumulator, currentValue) => accumulator + currentValue.length,
+  0
+)
