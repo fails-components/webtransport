e1b6ec83ecbdbc1181644d69d7230e5d4b1473e4
Marten Richter <marten.richter@freenet.de>
2024-11-23 06:51:22 +0100

    Delete transports/http3-quiche/platform/quiche_platform_impl/quiche_mutex_impl.h
    Delete unused mutex file
    
    Signed-off-by: Marten Richter <marten.richter@freenet.de>

--- transports/http3-quiche/platform/quiche_platform_impl/quiche_mutex_impl.h
+++
@@ -1,8 +0,0 @@
-// from envoy so LICENSE.envoy applies
-// use default impl
-#ifndef QUICHE_MUTEX_IMPL
-#define QUICHE_MUTEX_IMPL
-
-#include "third_party/quiche/quiche/common/platform/default/quiche_platform_impl/quiche_mutex_impl.h"
-
-#endif
